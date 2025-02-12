1b3b45a82219d093a685fd2c7930e773d185ddc5
Pavel Lazarev <pashga112@gmail.com>
2025-02-12 19:40:35 +0300

    Work done!

---
+++ main.py
@@ -0,0 +1,68 @@
+import file_operations, random, os
+from faker import Faker
+
+os.makedirs("cards", exist_ok=True)
+fake = Faker("ru_RU")
+
+skills = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд",
+]
+rune_letters = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+if __name__ == "__main__":
+    for card in range(10):
+        skills_sample = random.sample(skills, 3)
+        with open("charsheet.svg", "r") as cards:
+            runic_skills = []
+            for word in skills_sample:
+                for letter in word:
+                    if letter in rune_letters.keys():
+                        word = word.replace(letter, rune_letters[letter])
+                runic_skills.append(word)
+            context = {
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name(),
+                "job": fake.job(),
+                "town": fake.city(),
+                "strength": random.randint(3, 18),
+                "agility": random.randint(3, 18),
+                "endurance": random.randint(3, 18),
+                "intelligence": random.randint(3, 18),
+                "luck": random.randint(3, 18),
+                "skill_1": runic_skills[0],
+                "skill_2": runic_skills[1],
+                "skill_3": runic_skills[2],
+            }
+
+        file_operations.render_template("charsheet.svg", f"cards/result_{card+1}.svg", context)
